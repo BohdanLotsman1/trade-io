191f92b78c8d4b5c63bf23804b0aff7e3e796a36
BohdanLotsman1 <boglotsman@gmail.com>
2025-09-10 14:02:16 +0300

    updated history endpoint

--- src/modules/history/controller/history.controller.ts
+++ src/modules/history/controller/history.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { HistoryService } from '../service/history.service';
 
 @Controller('history')
@@ -8,21 +15,27 @@ export class HistoryController {
   @Get('/')
   async getHistory(
     @Query('currency') currency: string,
-    @Query('interval') interval: string,
+    @Query('interval') interval?: string,
+    @Query('endTime') endTime?: number,
   ) {
     try {
       const currencyString = currency.replace('/', '');
-      const history = await this.historyService.getHistory(
+      const history = await this.historyService.getHistory({
         currencyString,
         interval,
-      );
+        endTime,
+      });
+
+      if (!history.length) {
+        throw new BadRequestException(history);
+      }
       return {
         data: {
           history,
         },
       };
     } catch (e) {
-      return { message: 'Get history error' };
+      throw new BadRequestException('Get history error');
     }
   }
 }
--- src/modules/history/service/history.service.ts
+++ src/modules/history/service/history.service.ts
@@ -3,24 +3,27 @@ import axios from 'axios';
 
 @Injectable()
 export class HistoryService {
-  async getHistory(currencyString: string, interval: string) {
+  async getHistory({
+    currencyString,
+    interval,
+    endTime,
+  }: {
+    currencyString: string;
+    interval: string;
+    endTime: number;
+  }) {
     try {
       const array = await axios.get(
-        `https://www.binance.com/api/v3/klines?limit=1000&symbol=${currencyString}&interval=${interval}`,
+        `https://www.binance.com/api/v3/klines?limit=1000&symbol=${currencyString}&interval=${interval ?? '1m'}${endTime ? `&endTime=${endTime}` : ''}`,
       );
-
-      return array.data.map((item: Array<string>) => {
-        console.log('HISTORY', item);
-
-        return {
-          time: item[0],
-          open: Number(item[1]),
-          high: Number(item[2]),
-          low: Number(item[3]),
-          close: Number(item[4]),
-          volume: Number(item[5]),
-        };
-      });
+      return array.data.map((item: Array<string>) => ({
+        time: item[0],
+        open: Number(item[1]),
+        high: Number(item[2]),
+        low: Number(item[3]),
+        close: Number(item[4]),
+        volume: Number(item[5]),
+      }));
     } catch (e) {
       return e;
     }
